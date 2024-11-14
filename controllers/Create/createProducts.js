694cf7ecb20787587df10a548ac52767c8e2e8a0
Yusako90 <141408182+Yusako90@users.noreply.github.com>
2024-11-14 13:25:28 +0800

    gago

--- controllers/Create/createProducts.js
+++ controllers/Create/createProducts.js
@@ -1,5 +1,7 @@
+const Product = require('../../models/products');
 const createProduct = require('../../models/products');
 const express = require('express');
+const parse = require('../../models/promos');
 exports.createProducts = async (req, res, next) => {
     try {
         const { productName, description, price, discount, quantity, productType, imgUrl } = req.body;
@@ -19,5 +21,20 @@ exports.createProducts = async (req, res, next) => {
         next(err);
     }
 }
+
+exports.createPromos = async (req, res, next) => { 
+    try {
+        const { productName, expiry, discount } = req.body;
+        const promos = new parse({
+            productName: productName,
+            expiry: expiry,
+            discountOffer: discount,
+        })
+        res.status(201).json({ message: 'Promo created successfully', newPromo });
+    } catch (err) {
+        res.status(500).json({ message: `${err}` });
+        next(err);
+    }
+ }
 
 
